fe3947c0b9abc83544bf3ed0306119379e00ba84
Adam <adam.tyler.le@gmail.com>
2025-08-19 02:27:44 -0400

    CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,12 +9,15 @@ origins = [
     "http://localhost:5173",  
     "http://127.0.0.1:5173",
     "https://nbapredictor.vercel.app",
+    "https://nbapredictor-git-main-adamle24.vercel.app",
+
+
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
